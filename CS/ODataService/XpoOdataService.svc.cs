b6195c9680f5a47874dfc42b478780feb7c12c90
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/ODataService/XpoOdataService.svc.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using Northwind;
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using System.ServiceModel;
+using System.IO;
+using System.Web;
+using System.Data.Services.Common;
+using System.Linq.Expressions;
+using System.ServiceModel.Web;
+using System.Linq;
+using System.Web.Security;
+
+namespace XpoODataExample {
+    public class XpoODataContext : XpoContext {
+        public bool CategoriesFiltering = false;
+        public XpoODataContext(string s1, string s2, IDataLayer dataLayer)
+            : base(s1, s2, dataLayer) {
+        }
+        [Action]
+        public bool CanRemove(Products product) {
+            return !(new XPQuery<Suppliers>(product.Session)).Any(s => s.Products.Contains(product));
+        }
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class XpoODataExampleService : XpoDataServiceV3 {
+        static readonly XpoODataContext context = new XpoODataContext("XpoExampleContext", "Northwind", CreateDataLayer());
+
+        public XpoODataExampleService() : base(context, context) { }
+
+        static IDataLayer CreateDataLayer() {
+            DevExpress.Xpo.Metadata.XPDictionary dict = new DevExpress.Xpo.Metadata.ReflectionDictionary();
+            // Initialize the XPO dictionary. 
+            dict.GetDataStoreSchema(typeof(Orders).Assembly);
+            InMemoryDataStore store = new InMemoryDataStore(AutoCreateOption.SchemaOnly);
+            string DBFileName = DevExpress.Utils.FilesHelper.FindingFileName(HttpRuntime.AppDomainAppPath, "App_Data\\nwind.xml");
+            if(DBFileName != "")
+                store.ReadXml(DBFileName);
+            IDataLayer dataLayer = new ThreadSafeDataLayer(dict, store);
+            XpoDefault.DataLayer = dataLayer;
+            XpoDefault.Session = null;
+            return dataLayer;
+        }
+        protected override void OnStartProcessingRequest(ProcessRequestArgs args) {
+            base.OnStartProcessingRequest(args);
+        }
+        public static void InitializeService(DataServiceConfiguration config) {
+            config.SetEntitySetAccessRule("*", EntitySetRights.All);
+            config.DataServiceBehavior.AcceptAnyAllRequests = true;
+            config.UseVerboseErrors = true;
+            config.SetServiceOperationAccessRule("*", ServiceOperationRights.All);
+            config.SetServiceActionAccessRule("*", ServiceActionRights.Invoke);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+            config.DataServiceBehavior.AcceptProjectionRequests = true;
+            config.DataServiceBehavior.AcceptCountRequests = true;
+            config.AnnotationsBuilder = CreateAnnotationsBuilder(() => context);
+            config.DataServiceBehavior.AcceptReplaceFunctionInQuery = true;
+            config.DataServiceBehavior.AcceptSpatialLiteralsInQuery = true;
+            config.DisableValidationOnMetadataWrite = true;
+        }
+
+        [QueryInterceptor("Categories")]
+        public Expression<Func<Categories, bool>> OnQueryActors() {
+            if(context.CategoriesFiltering)
+                return o => o.CategoryName.Length > 12;
+            else
+                return o => true;
+        }
+
+        [ChangeInterceptor("Products")]
+        public void OnChangeProducts(Products product, UpdateOperations operations) {
+            if (Equals(operations, UpdateOperations.Change)) {
+                Products productInBase = GetEntityUpdated<Products>(product);
+                if (productInBase.ProductName != product.ProductName) {
+                    throw new DataServiceException(400, string.Format("A {0} cannot be modified.", product.ToString()));
+                }
+            } else if (Equals(operations, UpdateOperations.Delete)) {
+                throw new DataServiceException(400, "Products cannot be deleted.");
+            }
+        }
+
+        public override object Authenticate(ProcessRequestArgs args) {
+            int oid;
+            try {
+                string authCookie = args.OperationContext.RequestHeaders["userCookie"];
+                FormsAuthenticationTicket ticket = FormsAuthentication.Decrypt(authCookie);
+                oid = int.Parse(ticket.UserData);
+            } catch {
+                return null;
+            }
+            using(UnitOfWork uow = new UnitOfWork(Context.ObjectLayer)) {
+                Employees cont = uow.GetObjectByKey<Employees>(oid);
+                return cont != null ? "ok" : ":(";
+            }
+        }
+
+        public override LambdaExpression GetQueryInterceptor(Type entityType, object token) {
+            if(token == null) throw new DataServiceException(401, "Unauthorized access.");
+            if((string)token == "ok")
+                return (Expression<Func<object, bool>>)(o => true);
+            else {
+                return (Expression<Func<object, bool>>)(o => false);
+            }
+        }
+
+        [WebGet]
+        public string GetAuthorizationCookie(string name, string password) {
+            using(UnitOfWork uow = new UnitOfWork(Context.ObjectLayer)) {
+                List<Employees> contacts = new XPQuery<Employees>(uow).Where(i => i.LastName == name).ToList();
+                Employees contact = contacts.SingleOrDefault();
+
+                if(contact == null) return string.Empty;
+                FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(
+                    1,
+                    contact.LastName,
+                    DateTime.Now,
+                    DateTime.Now.AddMinutes(30),
+                    true,
+                    contact.EmployeeID.ToString());
+
+                string encryptedTicket = FormsAuthentication.Encrypt(ticket);
+                return encryptedTicket;
+            }
+        }
+
+        [WebGet]
+        public void SetCategoriesFiltering(bool state) {
+            context.CategoriesFiltering = state;
+        }
+    }
+}
---
+++ VB/ODataV3Example/ClientForm.Designer.vb
@@ -0,0 +1,186 @@
+﻿Namespace ODataV3Example.Northwind
+    Partial Public Class ClientForm
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.panel1 = New System.Windows.Forms.Panel()
+            Me.checkRemove = New System.Windows.Forms.Button()
+            Me.categoryInteceptor = New System.Windows.Forms.GroupBox()
+            Me.dontUse = New System.Windows.Forms.RadioButton()
+            Me.use = New System.Windows.Forms.RadioButton()
+            Me.entityToShow = New System.Windows.Forms.ComboBox()
+            Me.pictureBox = New System.Windows.Forms.PictureBox()
+            Me.AuthorizeButton = New System.Windows.Forms.Button()
+            Me.gclODataV3ServiceMain = New DevExpress.XtraGrid.GridControl()
+            Me.gvlODataV3Service = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.panel1.SuspendLayout()
+            Me.categoryInteceptor.SuspendLayout()
+            DirectCast(Me.pictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.gclODataV3ServiceMain, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.gvlODataV3Service, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' panel1
+            ' 
+            Me.panel1.Controls.Add(Me.checkRemove)
+            Me.panel1.Controls.Add(Me.categoryInteceptor)
+            Me.panel1.Controls.Add(Me.entityToShow)
+            Me.panel1.Controls.Add(Me.pictureBox)
+            Me.panel1.Controls.Add(Me.AuthorizeButton)
+            Me.panel1.Dock = System.Windows.Forms.DockStyle.Top
+            Me.panel1.Location = New System.Drawing.Point(0, 0)
+            Me.panel1.Name = "panel1"
+            Me.panel1.Size = New System.Drawing.Size(694, 112)
+            Me.panel1.TabIndex = 1
+            ' 
+            ' checkRemove
+            ' 
+            Me.checkRemove.Anchor = (CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+            Me.checkRemove.Enabled = False
+            Me.checkRemove.Location = New System.Drawing.Point(436, 28)
+            Me.checkRemove.Name = "checkRemove"
+            Me.checkRemove.Size = New System.Drawing.Size(98, 23)
+            Me.checkRemove.TabIndex = 7
+            Me.checkRemove.Text = "Check Removable"
+            Me.checkRemove.UseVisualStyleBackColor = True
+            ' 
+            ' categoryInteceptor
+            ' 
+            Me.categoryInteceptor.Anchor = (CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+            Me.categoryInteceptor.Controls.Add(Me.dontUse)
+            Me.categoryInteceptor.Controls.Add(Me.use)
+            Me.categoryInteceptor.Enabled = False
+            Me.categoryInteceptor.Location = New System.Drawing.Point(292, 12)
+            Me.categoryInteceptor.Name = "categoryInteceptor"
+            Me.categoryInteceptor.Size = New System.Drawing.Size(134, 62)
+            Me.categoryInteceptor.TabIndex = 6
+            Me.categoryInteceptor.TabStop = False
+            Me.categoryInteceptor.Text = "Interceptor on Category"
+            ' 
+            ' dontUse
+            ' 
+            Me.dontUse.AutoSize = True
+            Me.dontUse.Location = New System.Drawing.Point(6, 35)
+            Me.dontUse.Name = "dontUse"
+            Me.dontUse.Size = New System.Drawing.Size(77, 17)
+            Me.dontUse.TabIndex = 1
+            Me.dontUse.Text = "Do not use"
+            Me.dontUse.UseVisualStyleBackColor = True
+            ' 
+            ' use
+            ' 
+            Me.use.AutoSize = True
+            Me.use.Checked = True
+            Me.use.Location = New System.Drawing.Point(6, 16)
+            Me.use.Name = "use"
+            Me.use.Size = New System.Drawing.Size(44, 17)
+            Me.use.TabIndex = 0
+            Me.use.TabStop = True
+            Me.use.Text = "Use"
+            Me.use.UseVisualStyleBackColor = True
+            ' 
+            ' entityToShow
+            ' 
+            Me.entityToShow.Anchor = (CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+            Me.entityToShow.DisplayMember = "1"
+            Me.entityToShow.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+            Me.entityToShow.Enabled = False
+            Me.entityToShow.FormattingEnabled = True
+            Me.entityToShow.Location = New System.Drawing.Point(116, 30)
+            Me.entityToShow.Name = "entityToShow"
+            Me.entityToShow.Size = New System.Drawing.Size(170, 21)
+            Me.entityToShow.TabIndex = 5
+            ' 
+            ' pictureBox
+            ' 
+            Me.pictureBox.Anchor = (CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
+            Me.pictureBox.Location = New System.Drawing.Point(540, 3)
+            Me.pictureBox.Name = "pictureBox"
+            Me.pictureBox.Size = New System.Drawing.Size(150, 105)
+            Me.pictureBox.TabIndex = 0
+            Me.pictureBox.TabStop = False
+            ' 
+            ' AuthorizeButton
+            ' 
+            Me.AuthorizeButton.Location = New System.Drawing.Point(12, 28)
+            Me.AuthorizeButton.Name = "AuthorizeButton"
+            Me.AuthorizeButton.Size = New System.Drawing.Size(98, 23)
+            Me.AuthorizeButton.TabIndex = 1
+            Me.AuthorizeButton.Text = "Authorize"
+            Me.AuthorizeButton.UseVisualStyleBackColor = True
+            ' 
+            ' gclODataV3ServiceMain
+            ' 
+            Me.gclODataV3ServiceMain.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gclODataV3ServiceMain.EmbeddedNavigator.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+            Me.gclODataV3ServiceMain.Location = New System.Drawing.Point(0, 112)
+            Me.gclODataV3ServiceMain.MainView = Me.gvlODataV3Service
+            Me.gclODataV3ServiceMain.Name = "gclODataV3ServiceMain"
+            Me.gclODataV3ServiceMain.Size = New System.Drawing.Size(694, 334)
+            Me.gclODataV3ServiceMain.TabIndex = 3
+            Me.gclODataV3ServiceMain.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gvlODataV3Service})
+            ' 
+            ' gvlODataV3Service
+            ' 
+            Me.gvlODataV3Service.GridControl = Me.gclODataV3ServiceMain
+            Me.gvlODataV3Service.Name = "gvlODataV3Service"
+            Me.gvlODataV3Service.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.False
+            Me.gvlODataV3Service.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.False
+            Me.gvlODataV3Service.OptionsBehavior.AllowIncrementalSearch = True
+            Me.gvlODataV3Service.OptionsBehavior.Editable = False
+            ' 
+            ' ClientForm
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(694, 446)
+            Me.Controls.Add(Me.gclODataV3ServiceMain)
+            Me.Controls.Add(Me.panel1)
+            Me.MinimumSize = New System.Drawing.Size(710, 485)
+            Me.Name = "ClientForm"
+            Me.Text = "OData V3 Example"
+            Me.panel1.ResumeLayout(False)
+            Me.categoryInteceptor.ResumeLayout(False)
+            Me.categoryInteceptor.PerformLayout()
+            DirectCast(Me.pictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.gclODataV3ServiceMain, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.gvlODataV3Service, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private panel1 As System.Windows.Forms.Panel
+        Private WithEvents AuthorizeButton As System.Windows.Forms.Button
+        Private gclODataV3ServiceMain As DevExpress.XtraGrid.GridControl
+        Private WithEvents gvlODataV3Service As DevExpress.XtraGrid.Views.Grid.GridView
+        Private pictureBox As System.Windows.Forms.PictureBox
+        Private WithEvents entityToShow As System.Windows.Forms.ComboBox
+        Private categoryInteceptor As System.Windows.Forms.GroupBox
+        Private WithEvents dontUse As System.Windows.Forms.RadioButton
+        Private WithEvents use As System.Windows.Forms.RadioButton
+        Private WithEvents checkRemove As System.Windows.Forms.Button
+
+    End Class
+End Namespace
